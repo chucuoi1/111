c1d3345a799712c7d5d4b17d3934a912204a6aaa
chucuoi1 <khang140388@yahoo.com>
2021-04-10 16:07:53 +0900

    Update psy_centos8.sh

--- psy_centos8.sh
+++ psy_centos8.sh
@@ -56,9 +56,9 @@ stacksize 6291456
 flush
 auth strong
 
-#users vilas:CL:12345
+users vilas:CL:12345
 #users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-users {print $1 ":CL:" $2 " "}' ${WORKDATA})
+#users {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
 $(awk -F "/" '{print "auth strong\n" \
 "allow " $1 "\n" \
